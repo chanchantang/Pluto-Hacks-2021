305863ae4ded13401c1f355021ee6bd379cad611
paarthchhabra <chhabrapaarth1717@gmail.com>
2021-10-24 00:21:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+$(".hamburger-link a").on("click", function(e) {
+	e.preventDefault();
+
+	$(".fixed-nav").toggleClass("visible");
+	$(".burger-icon").toggleClass("talk-x");
+});
+
+$(window).on("scroll", function() {
+	if($(window).scrollTop() >= $(".app-about").offset().top) {
+		var i = 0;
+
+		var loop = setInterval(function() {
+			$(".desc-text.t"+ ++i).addClass("visible");
+			if(i >= 3) clearInterval(loop);
+		}, 200);
+	}
+});
+
+$("a, button").on("click", function(e) {
+	e.stopPropagation();
+
+	var $el = $(this);
+
+	if(!$el.hasClass("clickedOnce")) {
+		e.preventDefault();
+
+		responsiveVoice.speak($el.attr("data-click-text"));
+
+		setTimeout(function() {
+			$el.addClass("clickedOnce");
+		}, 200);
+	}
+});
+
+$("#hero button").on("click", function(e) {
+	if($(this).is(".clickedOnce")) {
+		e.preventDefault();
+
+		$("body").animate({
+			scrollTop: $("#twocards").offset().top
+		}, 500);
+	}
+});
+
+$("section").on("click", function(e) {
+	var $el = $(this);
+	responsiveVoice.cancel();
+	responsiveVoice.speak($el.text());
+});
